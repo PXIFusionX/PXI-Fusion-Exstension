2a41b5f1d0fbb66f95c358e599ec19bea85751ac
PXIFusionX <barron.samuel111@gmail.com>
2024-08-19 15:57:49 -0700

    d

--- popup.js
+++ popup.js
@@ -2,10 +2,17 @@ document.getElementById('open-prodigy').addEventListener('click', () => {
     chrome.tabs.create({ url: 'https://math.prodigygame.com/?launcher=true&code=*' });
   });
   
-  document.getElementById('copy-code').addEventListener('click', () => {
-    const code = document.getElementById('code');
+  document.getElementById('copy-console-code').addEventListener('click', () => {
+    const code = document.getElementById('console-code');
     code.select();
     document.execCommand('copy');
-    alert('Code copied to clipboard!');
+    alert('Console code copied to clipboard!');
+  });
+  
+  document.getElementById('copy-bookmark-code').addEventListener('click', () => {
+    const code = document.getElementById('bookmark-code');
+    code.select();
+    document.execCommand('copy');
+    alert('Bookmark code copied to clipboard!');
   });
   
